b17b91a6968391428ecb91704807399427f3487f
Pineapple-chicken <289662467@qq.com>
2017-12-31 16:23:27 +0800

    Update synchronous.go

--- entities/synchronous.go
+++ entities/synchronous.go
@@ -1,1 +1,16 @@
+package entities
 
+func Naive() [10]traveAgency.Info {
+
+	var ifs [10]traveAgency.Info
+	customers := traveAgency.GetCustomerDetails()
+	destinations := traveAgency.GetRecommendedDestinations(customers)
+
+	for index, dest := range destinations {
+		q := traveAgency.GetQuote(dest)
+		w := traveAgency.GetWeatherForecast(dest)
+		ifs[index] = traveAgency.Info{Destinations:dest, Quote:q, Weather:w}
+	}
+
+	return ifs
+}
