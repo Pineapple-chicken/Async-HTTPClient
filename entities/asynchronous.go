4fba78dc1d1ee5acd2cfb6e4024eebb4fab4f263
Pineapple-chicken <289662467@qq.com>
2017-12-31 16:23:47 +0800

    Update asynchronous.go

--- entities/asynchronous.go
+++ entities/asynchronous.go
@@ -1,1 +1,34 @@
+package entities
 
+func Optimized() [10]TraveAgency.Info {
+
+    var ifs [10]TraveAgency.Info
+    weathers := [10]chan TraveAgency.Weather{}
+    quotes := [10]chan TraveAgency.Quoting{}
+	customers := TraveAgency.GetCustomerDetails()
+	destinations := TraveAgency.GetRecommendedDestinations(customers)
+
+    for i := range weathers {
+		weathers[i] = make(chan TraveAgency.Weather)
+	}
+
+	for i := range quotes {
+		quotes[i] = make(chan TraveAgency.Quoting)
+	}
+
+	for i, d := range destinations {
+		go func() {
+			quotes[i] <- TraveAgency.GetQuote(d)
+		}()
+
+		go func() {
+			weathers[i] <- TraveAgency.GetWeatherForecast(d)
+		}()
+	}
+
+	for i, d := range destinations {
+		ifs[i] = TraveAgency.Info{Destinations:d, Quote:<-quotes[i], Weather:<-weathers[i]}
+	}
+
+	return ifs
+}
