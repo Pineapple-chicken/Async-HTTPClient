962bd5606104f362346ee7e4cda5b9c7bdc666d3
Pineapple-chicken <289662467@qq.com>
2017-12-31 16:24:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
+package main
 
+import (
+	"flag"
+	"entities"
+)
+
+var n, o bool
+
+func main()  {
+	flag.BoolVar(&n, "n", false, "Using naive approach")
+	flag.BoolVar(&o, "o", false, "Using optimized approach")
+	flag.Parse()
+	if n {
+		synchronous.Naive()
+	}
+	if o {
+		asynchronous.Optimized()
+	}
+}
